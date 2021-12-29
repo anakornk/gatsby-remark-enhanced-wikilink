5797746ac59cf7a0380cc096c3e7ddee70e7bdb7
Anakorn Kyavatanakij <anakornk@gmail.com>
2021-12-29 11:47:12 +0700

    feat: implement wikilink and embed images

---
+++ README.md
@@ -0,0 +1,63 @@
+# Gatsby Remark Enhanced Wikilink
+
+A Gatsby Remark plugin that adds support to Enhanced Wikilink Syntax (e.g., Obsidian Internal Links) for Gatsby ^4.0.0
+
+- To support file linking and image embedding, use with `gatsby-remark-images`, `gatsby-remark-copy-linked-files` and `gatsby-source-filesystem`
+- To be compatible with `gatsby-remark-autolink-headers`, the default implementation uses [Github Slugger](https://github.com/Flet/github-slugger) to slugify filenames and headings. This can be configured via setting a new `wikilinkToUrl` function. For more information, please see `Options` section below.
+
+## Supported Syntax
+
+- [x] Linking to MD files via `[[Internal Link#Heading | Alias]]`
+- [x] Linking to other files via `[[../path/document.pdf]]`
+- [x] Embed Images `![[../images/Hello.png]]`
+- [ ] Embed Notes `![[Internal Notes]]`
+
+## Installation
+
+```bash
+yarn add gatsby-remark-enhanced-wikilink
+```
+
+## Usage
+
+Add the plugin to your Gatsby config:
+
+```js
+// gatsby-config.js
+plugins: [
+    {
+        resolve: "gatsby-transformer-remark",
+        options: {
+            plugins: [
+                {
+                    resolve: 'gatsby-remark-obsidian',
+                    options: {
+                      stripBrackets: true,
+                      imageExtensions: ['png', 'jpg', 'jpeg'],
+                      fileFileExtensions: ['png', 'jpg', 'jpeg', 'pdf']
+                      // see other options below
+                    },
+                },
+            ]
+        }
+    },
+],
+```
+
+## Options
+
+```ts
+type WikilinkArgs = {
+  fileName?: string;
+  heading?: string;
+  alias?: string;
+};
+
+type Options = {
+  stripBrackets?: boolean;
+  wikilinkToUrl?: (args: WikilinkArgs) => string;
+  wikilinkToLinkText?: (args: WikilinkArgs) => string;
+  imageExtensions?: Array<string>;
+  linkFileExtensions?: Array<string>;
+};
+```
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,162 @@
 import visit from 'unist-util-visit';
-import toString from 'mdast-util-to-string';
+import { Node } from 'unist';
+import GithubSlugger from 'github-slugger';
+import type { Text } from 'mdast';
 
-export = async ({ markdownAST }: any) => {
-  visit(markdownAST, 'heading', (node: any) => {
-    let { depth } = node;
-    // Skip if not an h1
-    if (depth !== 1) return;
-    // Grab the innerText of the heading node
-    let text = toString(node);
-    const html = `
-        <h1 style="color: rebeccapurple">
-          ${text}
-        </h1>
-      `;
-    node.type = 'html';
-    node.children = undefined;
-    node.value = html;
-  });
-  return markdownAST;
+type WikilinkArgs = {
+  fileName?: string;
+  heading?: string;
+  alias?: string;
 };
+
+type Options = {
+  stripBrackets?: boolean;
+  wikilinkToUrl?: (args: WikilinkArgs) => string;
+  wikilinkToLinkText?: (args: WikilinkArgs) => string;
+  imageExtensions?: Array<string>;
+  linkFileExtensions?: Array<string>;
+};
+
+const slugify = GithubSlugger.slug;
+
+const defaultWikilinkToUrl: Options['wikilinkToUrl'] = ({
+  fileName,
+  heading,
+}) => {
+  return `${slugify(fileName)}${heading && `#${slugify(heading)}`}` || '#';
+};
+
+const defaultWikilinkToLinkText: Options['wikilinkToLinkText'] = ({
+  fileName,
+  heading,
+  alias,
+}) => {
+  return (
+    alias ||
+    (fileName && heading && `${fileName} > ${heading}`) ||
+    fileName ||
+    `> ${heading}`
+  );
+};
+
+const wrapWithBrackets = (text: string, isEmbed: boolean = false) => {
+  return isEmbed ? `![[${text}]]` : `[[${text}]]`;
+};
+
+const transformWikilink = (
+  { markdownAST }: { markdownAST: Node },
+  {
+    stripBrackets = true,
+    wikilinkToUrl = defaultWikilinkToUrl,
+    wikilinkToLinkText = defaultWikilinkToLinkText,
+    imageExtensions = ['png', 'jpg', 'jpeg'],
+    linkFileExtensions = ['png', 'jpg', 'jpeg', 'pdf'],
+  }: Options
+) => {
+  visit<Text>(markdownAST, 'text', (node, index, parent) => {
+    if (!parent) return;
+
+    const { value: text } = node;
+
+    const wikilinkRegExp = /(!?)\[\[(.+?)\]\]/g;
+
+    const result = [];
+    let start = 0;
+
+    let match = wikilinkRegExp.exec(text);
+
+    // loop each match and transform wikilinks
+    while (match) {
+      const position = match.index;
+      const fullMatchedString = match[0];
+      const isEmbed = match[1] === '!';
+      const wikilinkText = match[2];
+
+      if (start !== position) {
+        result.push({
+          type: 'text',
+          value: text.slice(start, position),
+        });
+      }
+
+      if (isEmbed) {
+        const imageRegExp = new RegExp(
+          `([^\\/]*)\\.(?:${imageExtensions.join('|')})$`,
+          'i'
+        );
+        const imageMatch = wikilinkText.match(imageRegExp);
+        if (imageMatch) {
+          result.push({
+            type: 'image',
+            url: wikilinkText,
+            alt: imageMatch[1] || wikilinkText,
+          });
+        } else {
+          // TODO:  Support embed MD
+          result.push({
+            type: 'text',
+            value: wrapWithBrackets(wikilinkText, true),
+          });
+        }
+      } else {
+        const linkFileRegExp = new RegExp(
+          `\\.(?:${linkFileExtensions.join('|')})$`,
+          'i'
+        );
+        if (wikilinkText.match(linkFileRegExp)) {
+          result.push({
+            type: 'link',
+            url: wikilinkText,
+            children: [
+              {
+                type: 'text',
+                value: stripBrackets
+                  ? wikilinkText
+                  : wrapWithBrackets(wikilinkText),
+              },
+            ],
+          });
+        } else {
+          // get file name, heading and alias
+          const splitRegex = /([^#\|]*)(?:#?)([^\|]*)(?:\|?)(.*)/; // split wikilink text to fileName, heading and alias
+          let [_, fileName, heading, alias] = wikilinkText.match(
+            splitRegex
+          ) || [,];
+          fileName = fileName?.replace(/\.md$/, '').trim(); // support filename with md ext
+          heading = heading?.trim();
+          alias = alias?.trim();
+
+          const url = wikilinkToUrl({ fileName, heading, alias });
+          const linkText = wikilinkToLinkText({ fileName, heading, alias });
+
+          result.push({
+            type: 'link',
+            url: url,
+            children: [
+              {
+                type: 'text',
+                value: stripBrackets ? linkText : wrapWithBrackets(linkText),
+              },
+            ],
+          });
+        }
+      }
+
+      start = position + fullMatchedString.length;
+      match = wikilinkRegExp.exec(text);
+    }
+
+    // if there is at least one match
+    if (result.length > 0) {
+      // add the rest of the text that hasn't been matched
+      if (start < text.length) {
+        result.push({ type: 'text', value: text.slice(start) });
+      }
+      // add siblings
+      parent.children.splice(index, 1, ...result);
+      return index + result.length;
+    }
+  });
+};
+
+export = transformWikilink;
